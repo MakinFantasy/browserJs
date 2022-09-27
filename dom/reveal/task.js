93fd2cee31c99d0c1910c5488c00d6fee7143ebf
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-09-27 13:05:50 +0300

    fixed reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,15 +2,13 @@ const popups = document.querySelectorAll('.reveal')
 
 
 window.addEventListener('scroll', (e) => {
-    console.log(e.type)
-    const {top, bottom} = popups[0].getBoundingClientRect();
-    console.log(top, bottom, window.innerHeight)
-    if (top <  window.innerHeight) {
-        popups[0].classList.add('reveal_active')
-    } 
-    if (bottom < 0 || top > window.innerHeight) {
-        popups[0].classList.remove('reveal_active')
-    }
+    popups.forEach( (popup) => {
+        const {top, bottom} = popup.getBoundingClientRect();
+        console.log(top, bottom, window.innerHeight)
+        if (top <  window.innerHeight) {
+            popup.classList.add('reveal_active')
+        }
+    })
 })
 
 
